8e1185db66c02179cdab611509f86d2653a95f15
Tyler Powers <gr4yscale@gmail.com>
2016-01-07 13:23:30 +0000

    fix entry point for android

---
+++ index.android.js
@@ -0,0 +1,10 @@
+import React from 'react-native'
+import Root from './src/root'
+// import RootShareApp from './src/rootShareApp'
+
+const {
+  AppRegistry,
+} = React
+
+AppRegistry.registerComponent('HavitApp', () => Root)
+// AppRegistry.registerComponent('HavitShareApp', () => RootShareApp)
