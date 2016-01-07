37fe028d4568dc7e73645720d3eff0e915d372ee
Tyler Powers <gr4yscale@gmail.com>
2016-01-07 14:54:14 +0000

    more android fixes

--- android/app/src/main/java/com/havit/MainActivity.java
+++ android/app/src/main/java/com/havit/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
                 .setInitialLifecycleState(LifecycleState.RESUMED)
                 .build();
 
-        mReactRootView.startReactApplication(mReactInstanceManager, "havit", null);
+        mReactRootView.startReactApplication(mReactInstanceManager, "HavitApp", null);
 
         setContentView(mReactRootView);
     }
