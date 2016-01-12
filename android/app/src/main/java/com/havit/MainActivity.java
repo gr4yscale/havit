5746e48c89dbe14f7916b41ea23e3de8d17080bf
Tyler Powers <gr4yscale@gmail.com>
2016-01-12 16:07:35 +0000

    upgrade android project for RN 0.18

--- android/app/src/main/java/com/havit/MainActivity.java
+++ android/app/src/main/java/com/havit/MainActivity.java
@@ -1,93 +1,56 @@
 package com.havit;
 
-import com.smixx.reactnativeicons.ReactNativeIcons;
-import java.util.Arrays;
-import com.smixx.reactnativeicons.IconFont;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 
-import android.app.Activity;
+import java.util.Arrays;
+import java.util.List;
+
+/** Native Modules */
+import com.smixx.reactnativeicons.ReactNativeIcons;
+import com.smixx.reactnativeicons.IconFont;
 import com.projectseptember.RNGL.RNGLPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.smixx.reactnativeicons.ReactNativeIcons;
-import android.os.Bundle;
-import android.view.KeyEvent;
 
-import com.facebook.react.LifecycleState;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactRootView;
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
+public class MainActivity extends ReactActivity {
 
-public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
-
-    private ReactInstanceManager mReactInstanceManager;
-    private ReactRootView mReactRootView;
-
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mReactRootView = new ReactRootView(this);
-
-        mReactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
-                .setBundleAssetName("index.android.bundle")
-                .setJSMainModuleName("index.android")
-                .addPackage(new MainReactPackage())
-                .addPackage(new RNGLPackage())
-                .addPackage(new ReactMaterialKitPackage())
-                .addPackage(new LinearGradientPackage())
-                .addPackage(new ReactNativeIcons())
-                .addPackage(new ReactNativeIcons(Arrays.asList(
-                        new IconFont("ion", "ionicons.ttf")
-                )))
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
-
-        mReactRootView.startReactApplication(mReactInstanceManager, "HavitApp", null);
-
-        setContentView(mReactRootView);
+    protected String getMainComponentName() {
+        return "HavitApp";
     }
 
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
     @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
-            mReactInstanceManager.showDevOptionsDialog();
-            return true;
-        }
-        return super.onKeyUp(keyCode, event);
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
     }
 
+   /**
+   * A list of packages used by the app. If the app uses additional views
+   * or modules besides the default ones, add more packages here.
+   */
     @Override
-    public void onBackPressed() {
-      if (mReactInstanceManager != null) {
-        mReactInstanceManager.onBackPressed();
-      } else {
-        super.onBackPressed();
-      }
-    }
-
-    @Override
-    public void invokeDefaultOnBackPressed() {
-      super.onBackPressed();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onPause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onResume(this, this);
-        }
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new RNGLPackage(),
+        new ReactMaterialKitPackage(),
+        new LinearGradientPackage(),
+        new ReactNativeIcons(),
+        new ReactNativeIcons(Arrays.asList(
+            new IconFont("ion", "ionicons.ttf")
+            )
+        )
+      );
     }
 }
