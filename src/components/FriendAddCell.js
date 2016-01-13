5097d95f4e659f2327ad346815f0d11b37569e93
Tyler Powers <gr4yscale@gmail.com>
2016-01-13 11:38:23 +0000

    skeleton UI for friends management screen

---
+++ src/components/FriendAddCell.js
@@ -0,0 +1,80 @@
+import React from 'react-native';
+import {Icon} from '../../node_modules/react-native-icons'
+
+let {
+  Component,
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+} = React
+
+class FriendAddCell extends Component {
+  handlePress() {
+    this.props.onPress(this.props.data)
+  }
+
+  renderText() {
+    return (
+      <Text style={[styles.cellText, {color:'#FF3B7F'}]}>
+        {this.props.text}
+      </Text>
+    )
+  }
+
+  renderSelectionIcon() {
+    return (
+      <Icon
+          name={'ion|ios-close-empty'}
+          size={30}
+          color={'#FF3B7F'}
+          style={styles.selectedIcon}
+      />
+    )
+  }
+
+  render() {
+    return (
+      <TouchableHighlight
+          onPress={() => this.handlePress()}
+          delayPressIn={0}
+          delayPressOut={0}
+          underlayColor={'#FFFFFF'}
+      >
+        <View style={styles.row}>
+          {this.renderText()}
+          {this.renderSelectionIcon()}
+        </View>
+      </TouchableHighlight>
+    )
+  }
+}
+
+let styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8,
+    marginBottom: 0.5,
+    opacity: 1.0,
+    backgroundColor: '#FFFECE',
+    height: 48,
+  },
+  cellText: {
+    color: '#333333',
+    fontWeight: '500',
+    fontSize: 24,
+  },
+  selectedIcon: {
+    position:'absolute',
+    right: 10,
+    marginLeft: 10,
+    width: 30,
+    height: 30,
+  },
+})
+
+// TOFIX: start using propTypes, and be generic!
+
+export default FriendAddCell
---
+++ src/containers/FriendAddContainer.js
@@ -0,0 +1,164 @@
+import React from 'react-native';
+// import {connect} from 'react-redux/native'
+// import {bindActionCreators} from 'redux'
+// import * as serverActions from '../redux/actions/serverActions'
+import {Actions} from '../../node_modules/react-native-router-flux'
+import FriendAddCell from '../components/FriendAddCell'
+
+let {
+  Component,
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableHighlight,
+  ListView,
+  TextInput
+} = React
+
+let deviceHeight = Dimensions.get('window').height;
+let deviceWidth = Dimensions.get('window').width;
+
+class FriendAddContainer extends Component {
+
+  constructor(props) {
+    super(props)
+
+    let ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    })
+    this.state = {
+      dataSource: ds.cloneWithRows(["yo", "yo yo yo yo", "yo again", "more yo", "yo ho ho"]),
+    }
+
+    //dispatch fetching cache of all users
+  }
+
+  signupButtonPressed() {
+    const {signup} = this.props
+    signup().then(() => {
+      Actions.MainContainer()
+      // TOFIX: explictly save our store here to avoid a race so that when the user reloads
+      // the app and the user data wasn't saved, they're stuck with sign in screen
+    })
+  }
+
+  // TOFIX: is this really the best way to handle this?
+  handleTextEntered(event) {
+    let text = event.nativeEvent.text
+    this.props.signupNext(text)
+    if (this.textInput) {
+      this.textInput.setNativeProps({text: ''})
+    }
+  }
+
+  renderSearchForm() {
+    return (
+        <View style={styles.searchFormContainer}>
+          <TextInput
+              style={[styles.inputs, {flex: 1,backgroundColor:'#FFAA11'}]}
+              autoCapitalize={'none'}
+              autoCorrect={false}
+              onSubmitEditing={(event) => this.handleTextEntered(event)}
+              ref={(component)=>this.textInput = component}
+          />
+          <TouchableHighlight
+              style={styles.button}
+              underlayColor="#99d9f4"
+              onPress={() => this.signupButtonPressed()}
+          >
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableHighlight>
+        </View>
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.bigText}>{`Enter a friend's email address to add them:`}</Text>
+        <View style={[{backgroundColor:'#5511ff'}]}>
+          {this.renderSearchForm()}
+        </View>
+        <View style={{flex: 1,backgroundColor:'#9900ff'}}>
+        <ListView
+            dataSource = {this.state.dataSource}
+            renderRow = {(data) => {
+              return (
+                <FriendAddCell
+                    text={data}
+                />
+              )
+            }}
+            initialListSize={20}
+            pageSize={4}
+            style = {styles.listView}
+        />
+        </View>
+      </View>
+    );
+  }
+}
+
+let styles = StyleSheet.create({
+  container: {
+    flex:1,
+    flexDirection: 'column',
+    backgroundColor:'#FF3B00',
+    height:deviceHeight - 62, // - 62 when custom navbar is show
+  },
+  searchFormContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor:'#ff00ff',
+    padding:8,
+  },
+  inputs: {
+    height: 40,
+    borderWidth: 0,
+    padding: 8,
+    color:'#FFFFFF',
+    fontFamily:'AvenirNext-Medium', // http://iosfonts.com/
+    fontSize: 20,
+    // fontWeight: '600',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#000000',
+    alignSelf: 'center',
+    fontFamily:'AvenirNext-Medium',
+    fontWeight: '400',
+  },
+  button: {
+    height: 40,
+    width: 40,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+
+  },
+  listView: {
+    backgroundColor: 'rgba(0,1,0,0)',
+    paddingTop: 20,
+    paddingLeft: 8,
+    paddingRight: 8,
+  },
+  bigText: {
+    fontFamily:'AvenirNext-Medium', // http://iosfonts.com/
+    fontSize: 20,
+    fontWeight: '600',
+    color:'#FFFFFF',
+    backgroundColor: '#003377',
+    padding: 8,
+  },
+})
+
+// export default connect(
+//   (state) => {
+//     return state
+//   },
+//   (dispatch) => {
+//     return bindActionCreators(serverActions, dispatch);
+//   }
+// )(FriendAddContainer)
+
+export default FriendAddContainer
