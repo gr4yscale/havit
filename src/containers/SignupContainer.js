6a29e11c9703d937059517bd82db700b47a9e2d7
Tyler Powers <gr4yscale@gmail.com>
2016-01-22 12:46:48 +0000

    use some common components in sign up container

--- src/containers/SignupContainer.js
+++ src/containers/SignupContainer.js
@@ -3,9 +3,14 @@ import {connect} from 'react-redux/native'
 import {bindActionCreators} from 'redux'
 import * as serverActions from '../redux/actions/serverActions'
 import {Actions} from '../../node_modules/react-native-router-flux'
-import BlackWhiteTripShader from '../gl/blackWhiteTripShader'
-import {Icon} from '../../node_modules/react-native-icons'
-import StyleSheet from '../stylesheets/debugStylesheet.js'
+// import BlackWhiteTripShader from '../gl/BlackWhiteTripShader'
+// import StyleSheet from '../stylesheets/debugStylesheet.js'
+
+import HVTTextInput from '../components/HVTTextInput'
+import HVTButton from '../components/HVTButton'
+import HVTIconButton from '../components/HVTIconButton'
+
+import style, {COLOR_2, COLOR_3} from '../stylesheets/styles'
 
 let {
   Component,
@@ -13,7 +18,6 @@ let {
   View,
   Text,
   Dimensions,
-  TextInput,
   TouchableOpacity,
   Platform,
 } = React
@@ -38,30 +42,12 @@ class SignupContainer extends Component {
   }
 
   // TOFIX: is this really the best way to handle this?
-  handleTextEntered(event) {
+  handleOnSubmitEditing(event) {
     let text = event.nativeEvent.text
     this.props.signupNext(text)
-    if (this.textInput) {
+    if (this.textInput && Platform.os === 'ios') {
       this.textInput.setNativeProps({text: ''})
     }
-  }
-
-  renderFormIfNeeded() {
-    if (!this.props.finished)
-      return (
-      <View style={styles.formContainer}>
-        <View style={styles.formShadow} />
-        <View style={styles.formContent}>
-          <TextInput
-              style={styles.inputs}
-              autoCapitalize={'none'}
-              autoCorrect={false}
-              onSubmitEditing={(event) => this.handleTextEntered(event)}
-              ref={(component)=>this.textInput = component}
-          />
-        </View>
-      </View>
-    )
   }
 
   renderFinishButtonIfNeeded() {
@@ -77,49 +63,40 @@ class SignupContainer extends Component {
     )
   }
 
-  renderSignupButton() {
-    return (
-      <TouchableOpacity
-          style={[styles.button, styles.signupButton]}
-          underlayColor="#99d9f4"
-          onPress={() => Actions.SignIn()}
-      >
-        <Text style={[styles.buttonText, styles.smallText, {textAlign: 'center'}]}>Click here to sign in if you already have an account.</Text>
-      </TouchableOpacity>
-    )
-  }
-
-  renderFancyShaderBackgroundIfIOS() {
-    if (Platform.OS === 'ios') {
-      return (<BlackWhiteTripShader width={deviceWidth} height={deviceHeight} pixelRatio={2} collapsable={false} />)
-    }
-  }
-
-  renderCloseButton() {
-    return (
-      <TouchableOpacity
-          onPress={() => Actions.MainContainer()}
-          style={{position: 'absolute', top: 30, left: 18, width: 30, height: 30, backgroundColor:'transparent'}}
-      >
-        <Icon
-            name={'ion|android-close'}
-            size={30}
-            color={'#FFFFFF'}
-            style={styles.closeIcon}
+  renderFormIfNeeded() {
+    if (!this.props.finished)
+      return (
+      <View style={styles.formContainer}>
+        <Text {...style('text.huge', [styles.bigText])} >{this.props.promptText}</Text>
+        <HVTTextInput
+            width={deviceWidth - 42}
+            height={40}
+            onSubmitEditing={(event) => this.handleOnSubmitEditing(event)}
+            ref={(component)=>this.textInput = component}
         />
-      </TouchableOpacity>
+      </View>
     )
   }
 
   render() {
     return (
       <View style={styles.container}>
-        {this.renderFancyShaderBackgroundIfIOS()}
-        <Text style={[styles.text, styles.bigText]}>{this.props.promptText}</Text>
         {this.renderFormIfNeeded()}
         {this.renderFinishButtonIfNeeded()}
-        {this.renderSignupButton()}
-        {this.renderCloseButton()}
+
+        <HVTButton
+            text={"Click here to sign up if you already have an account."}
+            onPress={() => Actions.SignIn()}
+            extraTouchableStyle={styles.signupButton}
+        />
+
+        <HVTIconButton
+            iconName="ion|android-close"
+            size={30}
+            color={COLOR_3}
+            onPress={() => Actions.MainContainer()}
+            extraTouchableStyle={styles.closeButton}
+        />
       </View>
     );
   }
@@ -128,88 +105,36 @@ class SignupContainer extends Component {
 let styles = StyleSheet.create({
   container: {
     flex:1,
-    backgroundColor:'#FF3B7F',
-    height:deviceHeight - 62, // - 62 when custom navbar is show
+    backgroundColor:COLOR_3,
+    height:deviceHeight,
   },
   formContainer: {
-    marginTop: 8,
-    marginLeft: 30,
-    marginRight: 30,
-    backgroundColor: '#FF1E63',
-  },
-  formContent: {
-    backgroundColor: '#FFFFFF',
-    position:'absolute',
-    width: deviceWidth - 60,
-    height: 40,
-  },
-  formShadow: {
-    backgroundColor:'#000000',
-    position:'absolute',
-    width: deviceWidth - 60,
-    height: 40,
-    marginLeft: 6,
-    marginTop: 6,
-    opacity: 0.3,
-  },
-  inputs: {
-    height: 40,
-    borderWidth: 0,
-    padding: 8,
-    color:'#000000',
-    fontFamily:'AvenirNext-Medium', // http://iosfonts.com/
-    fontSize: 16,
-    // fontWeight: '600',
-  },
-  text: {
-    fontFamily:'AvenirNext-Medium', // http://iosfonts.com/
-    fontWeight: '600',
-    color:'#FFFFFF',
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 100,
-    paddingBottom:4,
-    paddingTop:2,
-    paddingLeft: 4,
-    backgroundColor: '#000000',
-  },
-  bigText: {
-    fontSize: 24,
-  },
-  smallText: {
-    fontSize: 16,
-    fontWeight: '500',
-    color: '#FFFFFF',
-    backgroundColor:'#000000',
-    padding: 6,
-  },
-  buttonText: {
-    fontSize: 18,
-    color: '#000000',
-    alignSelf: 'center',
-    fontFamily:'AvenirNext-Medium',
-    fontWeight: '400',
-  },
-  button: {
-    height: 40,
-    backgroundColor: '#FFFFFF',
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    marginTop: 12,
-    marginLeft: 30,
-    marginRight: 30,
-  },
-  signupButton: {
-    marginTop: 160,
+    flex: 0,
+    height: 100, // label + text input
+    marginTop: 70,
+    marginLeft: 20,
+    marginRight: 18,
     backgroundColor: 'transparent',
   },
-  closeIcon: {
+  bigText: {
+    paddingBottom:4,
+    marginBottom: 4,
+    paddingTop:2,
+    paddingLeft: 4,
+    color:COLOR_2,
+    backgroundColor:COLOR_3,
+    textAlign: 'center',
+  },
+  signupButton: {
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  closeButton: {
     position:'absolute',
-    left: 0,
-    top: 0,
-    width: 30,
-    height: 30,
-    backgroundColor:'transparent',
+    top: 28,
+    left: 18,
+    backgroundColor:'rgba(0,0,0,0)',
   },
 })
 
