40d2ea7282ca8a0782641ea3d25c73e9e802479e
Tyler Powers <gr4yscale@gmail.com>
2016-08-06 17:45:28 +0200

    Update Android build tools to use ReactAndroid binaries from NPM, update entry point for Android app to use ReactApplication

---
+++ android/app/src/main/java/com/havit/MainApplication.java
@@ -0,0 +1,43 @@
+package com.havit;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+// Native modules
+import com.oblador.vectoricons.VectorIconsPackage;
+// import ca.jaysoo.activityandroid.ActivityAndroidPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return true;
+      // return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        // new ReceiveIntentPackage(),
+        new VectorIconsPackage()
+        // new ActivityAndroidPackage(this)
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+      return mReactNativeHost;
+  }
+}
